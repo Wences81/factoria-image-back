02a95117d09943e3c7b25154154bb08c035fea78
Wences81 <zhylinpochta@gmail.com>
2022-04-02 21:40:59 +0200

    back-fix4

--- controllers/images/removeImage.js
+++ controllers/images/removeImage.js
@@ -2,10 +2,10 @@ const { Image } = require('../../models')
 const { NotFound } = require('http-errors')
 
 const removeImage = async (req, res) => {
-    const { _id } = req.params
-    const result = await Image.findByIdAndRemove(_id)
+    const { id } = req.params
+    const result = await Image.findByIdAndRemove(id)
     if (!result) {
-        throw new NotFound(`Image with id=${_id} not found`)
+        throw new NotFound(`Image with id=${id} not found`)
     }
     res.json({
         status: 'success',
--- controllers/images/updateImageById.js
+++ controllers/images/updateImageById.js
@@ -2,10 +2,10 @@ const { Image } = require('../../models')
 const { NotFound } = require('http-errors')
 
 const updateImageById = async (req, res) => {
-    const { _id } = req.params
-    const result = await Image.findByIdAndUpdate(_id, req.body, { new: true })
+    const { id } = req.params
+    const result = await Image.findByIdAndUpdate(id, req.body, { new: true })
     if (!result) {
-        throw new NotFound(`Image with id=${_id} not found`)
+        throw new NotFound(`Image with id=${id} not found`)
     }
     res.json({
         status: 'success',
